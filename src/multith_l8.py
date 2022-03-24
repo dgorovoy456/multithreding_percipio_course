7bd7325f4e4c3d1f4d81d0dc694b345182131e8e
denys.horovyi <denys.horovyi@globallogic.com>
2022-03-24 10:11:06 +0100

    Lesson_9
    - added function(my_process_with_avoiding_dead_lock) with avoiding Deadlock in Deadlock situation  multith_l8

--- src/multith_l8.py
+++ src/multith_l8.py
@@ -36,8 +36,45 @@ data_two = 5
 # t1.join()
 # t2.join()
 
+# print('########################################')
 
-def my_process_without_dead_lock(lock_one, lock_two):
+# def my_process_without_dead_lock(lock_one, lock_two):
+#     global data_one
+#     global data_two
+#
+#     lock_one.acquire()
+#     print(threading.current_thread().name, ' incrementing data_one')
+#     data_one += 1
+#     time.sleep(1)
+#
+#     lock_two.acquire()
+#     print(threading.current_thread().name, ' incrementing data_two')
+#     data_two += 1
+#     time.sleep(1)
+#
+#     lock_one.release()
+#     lock_two.release()
+#
+#
+# lock_one = threading.Lock()
+# lock_two = threading.Lock()
+#
+# t1 = threading.Thread(target=my_process_without_dead_lock, args=(lock_one, lock_two))
+#
+# t2 = threading.Thread(target=my_process_without_dead_lock, args=(lock_one, lock_two))
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
+#
+# print('data_one: {0}'.format(data_one))
+# print('data_two: {0}'.format(data_two))
+
+# print('########################################')
+
+def my_process_with_avoiding_dead_lock(lock_one, lock_two):
     global data_one
     global data_two
 
@@ -46,21 +83,22 @@ def my_process_without_dead_lock(lock_one, lock_two):
     data_one += 1
     time.sleep(1)
 
+    lock_one.release()
+
     lock_two.acquire()
     print(threading.current_thread().name, ' incrementing data_two')
     data_two += 1
     time.sleep(1)
 
-    lock_one.release()
     lock_two.release()
 
 
 lock_one = threading.Lock()
 lock_two = threading.Lock()
 
-t1 = threading.Thread(target=my_process_without_dead_lock, args=(lock_one, lock_two))
+t1 = threading.Thread(target=my_process_with_avoiding_dead_lock, args=(lock_one, lock_two))
 
-t2 = threading.Thread(target=my_process_without_dead_lock, args=(lock_one, lock_two))
+t2 = threading.Thread(target=my_process_with_avoiding_dead_lock, args=(lock_two, lock_one))
 
 t1.start()
 t2.start()
